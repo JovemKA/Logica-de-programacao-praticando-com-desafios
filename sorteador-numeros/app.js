95b7b2ccbc79fc36a73495402d0d9fb3b9a2851f
Andrey Kaiky Reis Ferreira <Andrey.kaiky@outlook.com>
2024-07-11 21:31:52 -0300

    Otimizar a lógica de geração de números na função sortear().

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,18 +1,9 @@
 function sortear() {
-  let quantidade = parseInt(document.getElementById("quantidade").value);
-  let de = parseInt(document.getElementById("de").value);
-  let ate = parseInt(document.getElementById("ate").value);
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
 
-  let sorteados = [];
-  let numero;
-
-  for (let i = 0; i < quantidade; i++) {
-    numero = obterNumeroAleatorio(de, ate);
-    sorteados.push(numero);
-  }
-  alert(sorteados);
-}
-
-function obterNumeroAleatorio(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do número: ${de}`);
+    alert(`Até o número: ${ate}`);
 }
